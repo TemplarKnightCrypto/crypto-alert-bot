0664e4a3f8351b39cec62575af25dec7281e3faf
TemplarKnightCrypto <templarknightcrypto@gmail.com>
2025-08-07 15:29:22 -0500

    Fix: Control Tower v8.7.2”

--- main.py
+++ main.py
@@ -168,6 +168,7 @@ def run_flask():
 # ============================================
 
 intents = discord.Intents.default()
+intents.message_content = True 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # ============================================
